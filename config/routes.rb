ef31d4e269b81e21f3670f57ee728d404935fcef
Doug Barr <douglas.barr01@gmail.com>
2023-03-07 15:14:50 +0000

    x

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
   root to: "lists#index"
-  resources :lists
+  resources :lists, only: [:index, :show, :create, :new] do
+    resources :bookmarks, only: [:new, :create, :show]
+  end
 end
