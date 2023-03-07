88c1a8a84bce74fed6dfceec1c4202b2ff3a892f
Doug Barr <douglas.barr01@gmail.com>
2023-03-07 11:14:38 +0000

    list show page

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,16 +10,23 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to lists_path(@list)
+      redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def show
+    @list = List.find(params[:id])
+  end
+
+
+
+
   private
 
   def list_params
-   params.require.(:list).permit(:name)
+   params.require(:list).permit(:name)
   end
 
 end
