73a10672a2a0f21eeab1283abd46fc4081dcf934
Doug Barr <douglas.barr01@gmail.com>
2023-03-08 16:03:04 +0000

    Add feature to destroy list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,12 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
   def index
     @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
   end
 
   def new
@@ -16,15 +22,19 @@ class ListsController < ApplicationController
     end
   end
 
-  def show
-    @list = List.find(params[:id])
-    @bookmark = Bookmark.new
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
   end
 
   private
 
-  def list_params
-   params.require(:list).permit(:name)
+  def set_list
+    @list = List.find(params[:id])
   end
 
+  def list_params
+    params.require(:list).permit(:name, :photo)
+  end
+  
 end
