25e887785fccfee37b9b24abb813899871d9f88f
Doug Barr <douglas.barr01@gmail.com>
2023-03-08 15:28:41 +0000

    Added destroy bookmark action

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,6 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: :destroy
+  
   def new
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
@@ -15,9 +17,20 @@ class BookmarksController < ApplicationController
     end
   end
 
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+
   private
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
   end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
 end
